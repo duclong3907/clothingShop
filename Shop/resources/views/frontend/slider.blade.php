3c2693b3bfe72b0caa0e0f3ddd4b48ffd33a42ac
duclong3907 <duclongminhtan@gmail.com>
2023-08-05 22:46:57 +0700

    Use blade engine to modify user and admin interface

---
+++ Shop/resources/views/frontend/slider.blade.php
@@ -0,0 +1,91 @@
+<section class="slider_section ">
+    <div class="slider_bg_box">
+       <img src="{{ asset('themes/frontend/images/jisoo.jpg') }}" alt="">
+    </div>
+    <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+       <div class="carousel-inner">
+          <div class="carousel-item active">
+             <div class="container ">
+                <div class="row">
+                   <div class="col-md-7 col-lg-6 ">
+                      <div class="detail-box">
+                         <h1>
+                            <span>
+                              Hot Deals of
+                            </span>
+                            <br>
+                               The Week
+                         </h1>
+                         <p>
+                            Discover the hottest deals of the week that will leave you thrilled! Explore our handpicked selection of discounted items, ranging from electronics to fashion, and grab incredible savings while they last. Shop now and save!  
+                         </p>
+                         <div class="btn-box">
+                            <a href="" class="btn1">
+                            Shop Now
+                            </a>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+          <div class="carousel-item ">
+             <div class="container ">
+                <div class="row">
+                   <div class="col-md-7 col-lg-6 ">
+                      <div class="detail-box">
+                         <h1>
+                            <span>
+                               Sale 20% Off
+                            </span>
+                            <br>
+                              On Everything
+                         </h1>
+                         <p>
+                           Hurry and take advantage of our limited-time offer! Enjoy a whopping 20% discount on all products. From fashion to electronics, it's the perfect opportunity to grab your favorite items at unbeatable prices. Shop now and save!
+                         </p>
+                         <div class="btn-box">
+                            <a href="" class="btn1">
+                            Shop Now
+                            </a>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+          <div class="carousel-item">
+             <div class="container ">
+                <div class="row">
+                   <div class="col-md-7 col-lg-6 ">
+                      <div class="detail-box">
+                         <h1>
+                            <span>
+                            Limited Time 
+                            </span>
+                            <br>
+                            Special Discounts
+                         </h1>
+                         <p>
+                           Unlock exclusive savings with our limited time offers and special discounts. From flash sales to seasonal promotions, seize the opportunity to enjoy unbeatable prices on a wide range of products. Don't miss out!
+                         </p>
+                         <div class="btn-box">
+                            <a href="" class="btn1">
+                            Shop Now
+                            </a>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div class="container">
+          <ol class="carousel-indicators">
+             <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+             <li data-target="#customCarousel1" data-slide-to="1"></li>
+             <li data-target="#customCarousel1" data-slide-to="2"></li>
+          </ol>
+       </div>
+    </div>
+ </section>
